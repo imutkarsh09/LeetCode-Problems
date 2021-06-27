5c7284aceb098bb2a4233ed8d671eecdfb1f5421
imutkarsh09 <itsutkarsh2711@gmail.com>
2021-06-27 17:38:45 +0530

    Subarray with sum equals k added

---
+++ Arrays/Medium/Prob_560.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Prob_560 {
+    public int subarraySum(int[] nums, int k) {
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        mp.put(0,1);
+        int sum=0;
+        int cnt = 0;
+        for(int i=0;i<nums.length;i++)
+        {
+            sum+=nums[i];
+            int comp = sum-k;
+            if(mp.get(comp)!=null)
+            {
+                cnt=cnt+mp.get(comp);
+            }
+            mp.put(sum,mp.getOrDefault(sum,0)+1);
+        }
+        return cnt;
+    }
+}
