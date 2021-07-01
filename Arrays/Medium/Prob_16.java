45bdcb806b15a10d71086f7e3bdb6b0f88ec4ae4
imutkarsh09 <itsutkarsh2711@gmail.com>
2021-07-01 15:21:59 +0530

    Three Sum Closest added

---
+++ Arrays/Medium/Prob_16.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Prob_16 {
+    public int threeSumClosest(int[] nums, int target) {
+        int prevdiff = Integer.MAX_VALUE;
+        int ans = 0;
+        Arrays.sort(nums);
+        for(int i=0; i<nums.length-2; i++) {
+            if(i>0 && nums[i] == nums[i-1])continue;
+            int j = i+1;
+            int k = nums.length-1;
+            while(j<k) {
+                int sum = nums[i]+nums[j]+nums[k];
+                if(sum == target) {
+                    return sum;
+                } 
+                int diff = Math.abs(sum-target);
+                if(diff < prevdiff) {
+                    prevdiff = diff;
+                    ans=sum;
+                }
+                if(sum < target) j++;
+                else k--;
+            }
+        }
+        return ans;
+    }
+}
